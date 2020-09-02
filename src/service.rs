a9824e6232a987c34657baba686844d8516dc7be
Edouard Paris <m@edouard.paris>
2020-09-02 17:47:04 +0200

    fix(service): deps

--- src/service.rs
+++ src/service.rs
@@ -1,8 +1,6 @@
-extern crate bitcoin_hashes;
 extern crate hex;
 extern crate secp256k1;
 
-use bitcoin_hashes::{sha256, Hash};
 use secp256k1::{Error, Message, PublicKey, Secp256k1, Signature, Verification, VerifyOnly};
 
 /// VerifierError is the AuthVerifier errors.
@@ -47,43 +45,4 @@ pub fn verify_sig<C: Verification>(
     let pubkey = PublicKey::from_slice(pubkey)?;
     secp.verify(&msg, &sig, &pubkey)?;
     Ok(true)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    extern crate bitcoin_hashes;
-    use secp256k1::{Error, Message, Secp256k1, SecretKey, Signature, Signing};
-
-    fn sign<C: Signing>(
-        secp: &Secp256k1<C>,
-        msg: &[u8],
-        seckey: [u8; 32],
-    ) -> Result<Signature, Error> {
-        let msg = sha256::Hash::hash(msg);
-        let msg = Message::from_slice(&msg)?;
-        let seckey = SecretKey::from_slice(&seckey)?;
-        Ok(secp.sign(&msg, &seckey))
-    }
-
-    #[test]
-    fn test_verify_sig() {
-        let secp = Secp256k1::new();
-        let seckey = [
-            59, 148, 11, 85, 134, 130, 61, 253, 2, 174, 59, 70, 27, 180, 51, 107, 94, 203, 174,
-            253, 102, 39, 170, 146, 46, 252, 4, 143, 236, 12, 136, 28,
-        ];
-        let pubkey = [
-            2, 29, 21, 35, 7, 198, 183, 43, 14, 208, 65, 139, 14, 112, 205, 128, 231, 245, 41, 91,
-            141, 134, 245, 114, 45, 63, 82, 19, 251, 210, 57, 79, 54,
-        ];
-        let msg = b"This is some message";
-
-        let signature = sign(&secp, msg, seckey).unwrap();
-
-        let serialize_sig = signature.serialize_compact();
-
-        assert!(verify_sig(&secp, msg, &serialize_sig, &pubkey).unwrap());
-    }
 }
